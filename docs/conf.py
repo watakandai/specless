618d3f641091125f32400ad2b29b6ed422d8213d
Kandai Watanabe <kandai.wata@gmail.com>
2023-11-10 15:39:22 -0700

    fixed the package name in docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'package_name'
+project = 'specless'
 copyright = '2019, Kandai Watanabe'
 author = 'Kandai Watanabe'
 
