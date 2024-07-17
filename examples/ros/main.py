00ac501952ef20a170b1dca29e5d5d173f9a0037
Kandai Watanabe <kandai.wata@gmail.com>
2024-07-17 12:10:15 -0600

    fixed ros integration

--- examples/ros/main.py
+++ examples/ros/main.py
@@ -1,4 +1,6 @@
-from typing import Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
+
+import numpy as np
 
 import specless as sl
 
@@ -10,10 +12,11 @@ def get_node_assignments(
     nodes: List[int],
     costs: List[List[float]],
     initial_nodes: List[int],
-    num_agent: int = 1,
     global_constraints: Dict[int, Tuple[float, float]] = {},
     local_constraints: Dict[Tuple[int, int], Tuple[float, float]] = {},
 ):
+    num_agent: int = len(initial_nodes)
+
     # Define Time Specification
     tpo: sl.TimedPartialOrder = sl.TimedPartialOrder.from_constraints(
         global_constraints, local_constraints
@@ -28,53 +31,68 @@ def get_node_assignments(
         tsp_with_tpo, num_agent=num_agent, init_nodes=initial_nodes
     )
 
+    return tours, cost, timestamps
+
 
 def get_location_assignments(
     locations: List[Location],
-    costs: List[List[float]],
-    initial_locations: List[Location],
-    num_agent: int = 1,
-    global_constraints: Dict[Location, TimeBound] = {},
-    local_constraints: Dict[Tuple[Location, Location], TimeBound] = {},
+    initial_nodes: List[int],
+    costs: Optional[List[List[float]]] = None,
+    global_constraints: Dict[int, TimeBound] = {},
+    local_constraints: Dict[Tuple[int, int], TimeBound] = {},
 ):
     # Convert locations to nodes
     n: int = len(locations)
     nodes: List[int] = list(range(n))
     node_to_loc = {i: l for i, l in enumerate(locations)}
-    loc_to_node = {l: i for i, l in enumerate(locations)}
-    initial_nodes = [loc_to_node[l] for l in locations]
-    global_constraints_ = {loc_to_node[l]: tb for l, tb in global_constraints.items()}
-    local_constraints_ = {
-        (loc_to_node[s], loc_to_node[t]): tb for (s, t), tb in local_constraints.items()
-    }
+
+    if costs is None:
+        costs = [
+            [np.linalg.norm(np.array(l2) - np.array(l1)) for l1 in locations]
+            for l2 in locations
+        ]
 
     # Compute node assignments
     tours, cost, timestamps = get_node_assignments(
-        nodes, costs, initial_nodes, num_agent, global_constraints_, local_constraints_
+        nodes, costs, initial_nodes, global_constraints, local_constraints
     )
     # Convert node assignments to location assignments
-    location_assignments = list(map(lambda n: node_to_loc[n], tours))
+    location_assignments = [list(map(lambda n: node_to_loc[n], tour)) for tour in tours]
     return location_assignments, cost, timestamps
 
 
 def main():
     # Load files
 
-    # TODO: Define the number of agents
-    num_agent: int = 1
     # TODO: Define a list of locations
-    locations = []
-    # TODO: Define a list of travel costs between locations
-    costs = [[]]
+    locations = [(4, 4), (2, 0), (0, 2), (3, 3)]
+    initial_nodes = [0]
+
+    # TODO: OPTIONAL: Define a list of travel costs between locations if EUCLIDEAN distance is not used
+    # costs = [[]]
+
     # TODO: Define a list of global time constraints map at locations in the form of (LB, UB)
     global_constraints: Dict[Location, TimeBound] = {
         # ex) (1, 2): [0, 10] means a robot must reach at (1, 2) between 0 to 10 seconds since the simulation has started
+        0: (0, 100),
+        1: (5, 16),
+        2: (0, 10),
+        3: (8, 14),
     }
 
     # TODO: Define a list of local time constraints between locations in the form of (LB, UB)
     local_constraints: Dict[Tuple[Location, Location], TimeBound] = {
         # ex) ((1, 2), (5, 4)): [10, 20] means from location (1, 2) to (5, 4) it must reach in between 10 to 20 seconds
+        (1, 2): [3, 7],
     }
+
     get_location_assignments(
-        locations, costs, num_agent, global_constraints, local_constraints
+        locations,
+        initial_nodes,
+        global_constraints=global_constraints,
+        local_constraints=local_constraints,
     )
+
+
+if __name__ == "__main__":
+    main()
